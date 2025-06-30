955512de01ca8fe9defd405fa17f5f3cae40ebfc
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2025-06-30 04:35:46 +0300

    add watermark

--- src/nodes/save_image_s3.py
+++ src/nodes/save_image_s3.py
@@ -1,3 +1,4 @@
+import io
 import os
 import tempfile
 import numpy as np
@@ -8,10 +9,12 @@ from ..client_s3 import get_s3_instance
 from ..common.generator import id_generator
 from ..logger import logger
 
+WATERMARK_IMAGE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "watermark.png")
 S3_INSTANCE = get_s3_instance()
 
 
 class NextrySaveImageS3:
+    
     def __init__(self):
         base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
         self.temp_dir = os.path.join(base_dir, "temp/")
@@ -19,6 +22,8 @@ class NextrySaveImageS3:
         self.type = "output"
         self.prefix_append = ""
         self.compress_level = 4
+        with open(WATERMARK_IMAGE_PATH, "rb") as f:
+            self.watermark_bytes = f.read()
 
     @classmethod
     def INPUT_TYPES(cls):
@@ -27,6 +32,7 @@ class NextrySaveImageS3:
                 "images": ("IMAGE", ),
                 "filename_prefix": ("STRING", {"default": "Image"}),
                 "s3_bucket_name": ("STRING", {"default": None}),
+                #"add_watermark": ("BOOLEAN", {"default": True}),
             }
         }
 
@@ -37,7 +43,23 @@ class NextrySaveImageS3:
     OUTPUT_IS_LIST = (True,)
     CATEGORY = "NEXTRY_ComfyS3"
 
-    def save_images(self, images, filename_prefix="ComfyUI", s3_bucket_name=None):
+    def apply_watermark(self, img: Image.Image, margin: int = 10) -> Image.Image:
+    
+        base = img.convert("RGBA")
+        icon = Image.open(io.BytesIO(self.watermark_bytes)).convert("RGBA")
+     
+        max_w = base.width // 6
+        scale = max_w / icon.width
+        resample = Image.Resampling.LANCZOS
+      
+        icon = icon.resize((int(icon.width * scale), int(icon.height * scale)), resample)
+        pos = (base.width - icon.width - margin, base.height - icon.height - margin)
+        base.paste(icon, pos, icon)
+        return base
+
+
+    def save_images(self, images, filename_prefix="Nextry", s3_bucket_name=None):
+        logger.info(f"Input params: {images}, {filename_prefix}, {s3_bucket_name}")
         filename_prefix += self.prefix_append
         full_output_folder, filename, counter, subfolder, filename_prefix = S3_INSTANCE.get_save_path(filename_prefix, images[0].shape[1], images[0].shape[0])
         results = list()
@@ -46,6 +68,7 @@ class NextrySaveImageS3:
         for image in images:
             i = 255. * image.cpu().numpy()
             img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+            
             file_id = id_generator()
             file_full = f"stock_{file_id}.png"
             file_preview = f"preview_{file_id}.webp"
@@ -95,6 +118,7 @@ class NextrySaveImageS3:
         return { "ui": { "images": results },  "result": s3_image_paths }
 
     def save_temp_image(self, img: Image, suffix: str = ".png") -> str:
+        img = self.apply_watermark(img)
         with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as temp_file:
             temp_file_path = temp_file.name
             format_file = suffix.replace(".", "").upper()
@@ -121,7 +145,8 @@ class NextrySaveImageS3:
             img.save(temp_file_path, **save_kwargs)
 
             return temp_file_path
-
+        
+    
 
 
 
